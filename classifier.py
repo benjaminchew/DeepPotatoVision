662f53dea6c9f3ea83685770a294023e91770630
Benjamin Chew <ben.jh.chew@gmail.com>
2019-07-12 14:52:58 -0400

    Added img_process
    Files uploaded by the user have to be transformed to a tensor for Pytorch

--- classifier.py
+++ classifier.py
@@ -4,6 +4,8 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from load_imglist import ImageList, rgb_loader
 import torch.nn.functional as F
+from torch.autograd import Variable
+from PIL import Image
 
 savefile_model = 'classifier_model/vgg16_pretrained.pt'
 vgg16 = models.vgg16(pretrained=True)
@@ -118,3 +120,17 @@ def train_classifier(epochs=10, cmdline_args=None):
     file_trained_model = dir+'trained_classifier.pt'
     torch.save(model.state_dict(), file_trained_model)
     return logs_train
+
+def img_process(image_file):
+    
+    image_file = Image.open(image_file)
+
+    min_img_size = 224  # Size for PyTorch's pretrained models.
+    transform_pipeline = transforms.Compose([transforms.Resize(min_img_size),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                              std=[0.229, 0.224, 0.225])])
+    img = transform_pipeline(image_file)
+    img = img.unsqueeze(0)
+    img = Variable(img)
+    return img
